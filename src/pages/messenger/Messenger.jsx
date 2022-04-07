4ce7850452dc6f13f985272de9d3be3215fa4458
hackybacky <hacky3244@gmail.com>
2022-04-07 23:00:48 +0530

    commit

--- src/pages/messenger/Messenger.jsx
+++ src/pages/messenger/Messenger.jsx
@@ -21,7 +21,7 @@ export default function Messenger() {
   const scrollRef = useRef();
 
   useEffect(() => {
-    socket.current = io("ws://https://salty-temple-86005.herokuapp.com");
+    socket.current = io("https://salty-temple-86005.herokuapp.com");
     socket.current.on("getMessage", (data) => {
       setArrivalMessage({
         sender: data.senderId,
