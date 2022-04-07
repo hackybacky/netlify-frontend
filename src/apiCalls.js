013d17ad4f7b04718ffa6d81b4720c28510dc835
hackybacky <hacky3244@gmail.com>
2022-04-07 21:04:50 +0530

    commit

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 export const loginCall = async (userCredential, dispatch) => {
     dispatch({ type: "LOGIN_START" });
     try {
-        const res = await axios.post("auth/login", userCredential);
+        const res = await axios.post("https://safe-shore-72480.herokuapp.com/api/auth/login", userCredential);
         dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
     } catch (err) {
         dispatch({ type: "LOGIN_FAILURE", payload: err });
